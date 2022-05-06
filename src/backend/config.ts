e908faf34df85b7795b9f321a11442fd0674e551
lakeside763 <lakeside763@gmail.com>
2022-05-06 15:51:28 +0100

    chore: setting up server page

---
+++ src/backend/config.ts
@@ -0,0 +1,13 @@
+const port = process.env.PORT;
+const redisPort: number = parseInt(process.env.REDIS_HOST!)
+const redisHost: string = process.env.REDIS_HOST!;
+
+const redis = {
+    port: redisPort,
+    host: redisHost
+};
+
+export default {
+    port,
+    redis
+};
