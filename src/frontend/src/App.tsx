dff0611b5e6b545ebbf61f1a34228202d304cf36
lakeside763 <lakeside763@gmail.com>
2022-05-07 10:23:41 +0100

    chore: add navigation component

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom'
+import People from './components/people/people.component';
+import Person from './components/person/person.component';
+import Navigation from './routes/navigation.component';
 
 function App() {
   return (
-    <div className="App">
-      Welcome
-    </div>
+    <Routes>
+      <Route path='/' element={<Navigation />} >
+        <Route index element={<People />} />
+        <Route path='/people/:id' element={<Person />} />
+      </Route>
+    </Routes>
   );
 }
 
